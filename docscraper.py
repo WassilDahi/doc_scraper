beaca03ffe1fa5ada1487d0b2286c2108c7711f9
wassil <work.wassdahi@gmail.com>
2020-02-04 17:04:20 +0100

    //

--- docscraper.py
+++ docscraper.py
@@ -8,6 +8,10 @@ import urllib3
 import shutil
 import ssl
 from w3lib.url import safe_url_string
+import requests
+from PyPDF2 import PdfFileReader
+from socket import error as SocketError
+import errno
 
 
 
@@ -44,6 +48,8 @@ def scrap_all(urls_file):
 def scrap_urls(url):
     urls_list=[]
     url=re.sub(r'(\/\/)$','/',url)
+    if(re.match(r'.+\.pdf',str(url))):
+        return []
     if(re.match(r'^http[s]?:\/\/',str(url))):
         print("\n\n------------ Getting Urls from :  "+url)
         url = safe_url_string(url, encoding="utf-8")
@@ -86,10 +92,13 @@ def build_url(domain,url):
 
 
 def build_multiple_urls(domain,urls):
-    
-    return list(set([build_url(domain,url) for url in urls]))
+    domain2=re.sub(r'(\.\w+)\/.+',r'\g<1>',domain)
+    multiple_urls=list(set([build_url(domain,url) for url in urls]))
+    multiple_urls2=list(set([build_url(domain2,url) for url in urls]))
+    return set(multiple_urls+multiple_urls2)
 
 def download_doc(domain,url):
+    exist=0
     url=re.sub(r'(\/\/)$','/',url)
     doc_name=re.sub(r'.+?\/+','',url)
     domain_name=re.sub(r'^http[s]?:\/\/','',domain)
@@ -99,14 +108,15 @@ def download_doc(domain,url):
     
     folder_path='download'+'/'+domain_name
     doc_path=folder_path+'/'+doc_name
-
+    
     print("--- Downloading : "+ doc_name)
     try:
         # Create target Directory
         os.mkdir('download')
         print("Directory " , 'download' ,  " Created ") 
     except FileExistsError:
-        print("Directory " , 'download' ,  " already exists")      
+        print("Directory " , 'download' ,  " already exists")
+        exist=True      
     try:
         # Create target Directory
         os.mkdir(folder_path)
@@ -115,17 +125,26 @@ def download_doc(domain,url):
         print("Directory " , folder_path ,  " already exists")
     
     if(not os.path.isfile(doc_path)):
-        http = urllib3.PoolManager()
-        url = safe_url_string(url, encoding="utf-8")
-        with http.request('GET',url, preload_content=False) as resp, open(doc_path, 'wb') as out_file:
-            shutil.copyfileobj(resp, out_file)
+        #http = urllib3.PoolManager()
+        print(url)
 
-        resp.release_conn()
+        
+        try:
+            url = safe_url_string(url, encoding="utf-8")
+            '''with http.request('GET',url, preload_content=False, cert_reqs='CERT_NONE',
+                                assert_hostname=False) as resp, open(doc_path, 'wb') as out_file:
+                shutil.copyfileobj(resp, out_file)'''
+            urllib.request.urlretrieve(url,doc_path)
+        #except (urllib3.exceptions.HTTPError,urllib3.exceptions.MaxRetryError) as err:
+            #print(err)
+        except (urllib.error.HTTPError,urllib.error.URLError,SocketError) as err:
+            print(err)
+
+
+        #resp.release_conn()
     else:
         print(doc_name+' already exists')
-    #request.urlretrieve(url, domain_name+'/'+doc_name)
-    #wget.download(url,out=domain_name)
-
+            
 
 def downlaod_docs(url,docs):
     if (url.endswith('.pdf')):
